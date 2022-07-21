8d5ff9ccbd4cd2eb21baa9892d9be9e56eebff08
chico <chico@DESKTOP-VTBNL8C>
2022-07-21 17:01:58 -0300

    bd

--- src/main/java/dao/SqlConnection.java
+++ src/main/java/dao/SqlConnection.java
@@ -16,9 +16,7 @@ public class SqlConnection {
 		
 		try {
 			conn = DriverManager.getConnection(
-					"jdbc:postgresql://"
-					//+ "localhost:3306/stilingue", "root", "#Martina2010"
-					+ "ec2-54-152-28-9.compute-1.amazonaws.com:5432/de2eqcffklpr2i", "kwsndnbhgencnj", "9c0219c49efd67accd93746238ba920d780a4f177f79e2fb94b9fa646cca4a1a"
+					"jdbc:postgresql://ec2-54-152-28-9.compute-1.amazonaws.com:5432/de2eqcffklpr2i", "kwsndnbhgencnj", "9c0219c49efd67accd93746238ba920d780a4f177f79e2fb94b9fa646cca4a1a"
 					);
 		}catch (Exception e) {
 			e.printStackTrace();
